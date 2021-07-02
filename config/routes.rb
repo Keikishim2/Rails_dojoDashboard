068dc4cc2da3c24eda673d690d664b47883834d5
Keikishim2 <Keikishim2@gmail.com>
2021-07-02 12:48:07 +0800

    third

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :dojo_controller
   get '/' => "dojo_controller#index"
+  get '/dojos/new' => "dojo_controller#new"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
